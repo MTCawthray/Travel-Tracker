fc47b07058061423bf5a0a45e76ade1f966db36d
Mark Cawthray <dr.cawthray@gmail.com>
2021-08-04 10:08:09 -0600

    Add icons to header list items and style accordingly

--- src/scripts.js
+++ src/scripts.js
@@ -5,6 +5,12 @@
 import './css/base.scss';
 
 // An example of how you tell webpack to use an image (also need to link to it in the index.html)
-import './images/turing-logo.png'
+import './images/turing-logo.png';
+import './images/passport.png';
+import './images/confirm.png';
+import './images/airplane.png';
+import './images/suitcase.png';
+import './images/map.png';
+import './images/destination.png';
 
 console.log('This is the JavaScript entry file - your code begins here.');
