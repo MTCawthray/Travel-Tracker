fbe84e9488dcb004bef66b451cc0477db2fad501
Mark Cawthray <dr.cawthray@gmail.com>
2021-08-05 09:13:31 -0600

    Create small sample set of travelers

--- test/sample-test.js
+++ test/sample-test.js
@@ -1,8 +1,25 @@
 import chai from 'chai';
+import Traveler from '../src/Traveler.js';
+import sampleTravelers from '../src/sample-data/sample-traveler-data.js';
 const expect = chai.expect;
 
-describe('See if the tests are running', function() {
-  it('should return true', function() {
-    expect(true).to.equal(true);
+describe('Traveler class', () => {
+let travelersData, traveler1;
+
+  beforeEach(() => {
+    travelersData = sampleTravelers;
+    traveler1 = new Traveler(travelersData);
   });
+
+  it('should be a function', () => {
+    expect(Traveler).to.be.a('function');
+  });
+
+  it('should be an instance of the Traveler class', () => {
+    expect(traveler1).to.be.an.instanceOf(Traveler);
+  })
+
+  it('should hold travelers data', () => {
+    expect(traveler1.travelersData).to.eql(travelersData);
+  })
 });
