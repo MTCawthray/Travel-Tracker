a90b8baa85d57b521be7e93fafa387e3b397c565
Mark Cawthray <dr.cawthray@gmail.com>
2021-08-05 16:21:08 -0600

    Add trips and destinations properties to traveler class constructor and test

--- src/Traveler.js
+++ src/Traveler.js
@@ -1,8 +1,10 @@
 class Traveler {
-  constructor(travelerData) {
+  constructor(travelerData, trips, destinations) {
     this.id = travelerData.id;
     this.name = travelerData.name;
     this.type = travelerData.travelerType;
+    this.travelerTrips = trips;
+    this.travelerDestinations = destinations;
   }
 }
 
