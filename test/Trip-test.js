727bd7e4c61cd4a8bbcf79c807dbf5cbd35dca29
Mark Cawthray <dr.cawthray@gmail.com>
2021-08-05 16:47:35 -0600

    Add imports to trip test file

---
+++ test/Trip-test.js
@@ -0,0 +1,13 @@
+import chai from 'chai';
+const expect = chai.expect;
+import Traveler from '../src/Traveler.js';
+import Agency from '../src/Agency.js';
+import Trip from '../src/Trip.js';
+import sampleTrips from '../src/sample-data/sample-trips-data.js';
+import sampleDestinations from '../src/sample-data/sample-destinations-data.js'
+import sampleTravelers from '../src/sample-data/sample-traveler-data.js';
+
+describe('Trip class' () => {
+
+
+});
