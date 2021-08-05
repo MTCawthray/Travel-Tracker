f99826f747cbea158eda080e4ed63bc7555f89cd
Mark Cawthray <dr.cawthray@gmail.com>
2021-08-05 09:39:13 -0600

    Populate sample data files with sample data from api

---
+++ src/sample-data/sample-destinations-data.js
@@ -0,0 +1,99 @@
+const sampleDestinations = [
+  {
+    "id": 28,
+    "destination": "San Juan, Puerto Rico",
+    "estimatedLodgingCostPerDay": 70,
+    "estimatedFlightCostPerPerson": 900,
+    "image": "https://images.unsplash.com/photo-1580237541049-2d715a09486e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2090&q=80",
+    "alt": "white and brown concrete buildings near sea under white clouds during daytime"
+  },
+  {
+    "id": 10,
+    "destination": "Toronto, Canada",
+    "estimatedLodgingCostPerDay": 90,
+    "estimatedFlightCostPerPerson": 450,
+    "image": "https://images.unsplash.com/photo-1535776142635-8fa180c46af7?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2756&q=80"
+  },
+  {
+    "id": 6,
+    "destination": "Jakarta, Indonesia",
+    "estimatedLodgingCostPerDay": 70,
+    "estimatedFlightCostPerPerson": 890,
+    "image": "https://images.unsplash.com/photo-1555333145-4acf190da336?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80",
+    "alt": "lit up city at night"
+  },
+  {
+    "id": 7,
+    "destination": "Paris, France",
+    "estimatedLodgingCostPerDay": 100,
+    "estimatedFlightCostPerPerson": 395,
+    "image": "https://images.unsplash.com/photo-1524396309943-e03f5249f002?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1567&q=80",
+    "alt": "city during the day time with eiffel tower"
+  },
+  {
+    "id": 43,
+    "destination": "Nassau, The Bahamas",
+    "estimatedLodgingCostPerDay": 550,
+    "estimatedFlightCostPerPerson": 90,
+    "image": "https://images.unsplash.com/photo-1548574505-5e239809ee19?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1664&q=80",
+    "alt": "aerial photography of white and blue cruise ships during daytime"
+  },
+  {
+    "id": 20,
+    "destination": "Miami, Florida",
+    "estimatedLodgingCostPerDay": 158,
+    "estimatedFlightCostPerPerson": 275,
+    "image": "https://images.unsplash.com/photo-1514214246283-d427a95c5d2f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1573&q=80",
+    "alt": "sand with palm trees and tall buildings in the background"
+  },
+  {
+    "id": 22,
+    "destination": "Rome, Italy",
+    "estimatedLodgingCostPerDay": 90,
+    "estimatedFlightCostPerPerson": 650,
+    "image": "https://images.unsplash.com/photo-1515542622106-78bda8ba0e5b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80",
+    "alt": "people standing inside a colosseum during the day"
+  },
+  {
+    "id": 25,
+    "destination": "New York, New York",
+    "estimatedLodgingCostPerDay": 175,
+    "estimatedFlightCostPerPerson": 200,
+    "image": "https://images.unsplash.com/photo-1496442226666-8d4d0e62e6e9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80",
+    "alt": "people crossing the street during the day surrounded by tall buildings and advertisements"
+  },
+  {
+    "id": 16,
+    "destination": "Bangkok, Thailand",
+    "estimatedLodgingCostPerDay": 35,
+    "estimatedFlightCostPerPerson": 988,
+    "image": "https://images.unsplash.com/photo-1563492065599-3520f775eeed?ixlib=rb-1.2.1&auto=format&fit=crop&w=1567&q=80",
+    "alt": "ornate buildings with a garden during the day"
+  },
+  {
+    "id": 15,
+    "destination": "Manila, Philippines",
+    "estimatedLodgingCostPerDay": 40,
+    "estimatedFlightCostPerPerson": 900,
+    "image": "https://images.unsplash.com/photo-1555557356-51c5d7a8f4c2?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80",
+    "alt": "colorful buildings near the water with docked boats"
+  },
+  {
+    "id": 44,
+    "destination": "Caye Caulker, Belize",
+    "estimatedLodgingCostPerDay": 450,
+    "estimatedFlightCostPerPerson": 80,
+    "image": "https://images.unsplash.com/photo-1544525977-0a3bca9e560d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1500&q=80",
+    "alt": "boat on dock during daytime"
+  },
+  {
+    "id": 9,
+    "destination": "Amsterdam, Netherlands",
+    "estimatedLodgingCostPerDay": 100,
+    "estimatedFlightCostPerPerson": 950,
+    "image": "https://images.unsplash.com/photo-1534351590666-13e3e96b5017?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80",
+    "alt": "canal with boats and trees and buildings along the side"
+  },
+]
+
+export default sampleDestinations;
