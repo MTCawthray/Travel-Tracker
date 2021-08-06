c0da2c5116280c9d9f40ecb9d0d7634ab11523b1
Mark Cawthray <dr.cawthray@gmail.com>
2021-08-05 18:24:59 -0600

    Move the get trip cost method from agency class to traveler class

--- src/Traveler.js
+++ src/Traveler.js
@@ -6,6 +6,24 @@ class Traveler {
     this.travelerTrips = trips;
     this.travelerDestinations = destinations;
   }
+ 
+  getTripCost(tripID) {
+    if (this.travelerTrips.find(trip => trip.id === tripID)) {
+      let bookedTrip = this.travelerTrips.find(trip => trip.id === tripID);
+      let destination = this.travelerDestinations.find(place => place.id === bookedTrip.destinationID);
+      let lodgingCost = destination.estimatedLodgingCostPerDay;
+      let flightCost = destination.estimatedFlightCostPerPerson;
+      let agentFee = (lodgingCost + flightCost) * .1;
+      let costBreakdown = {
+        flight: flightCost,
+        lodging: lodgingCost,
+        fee: agentFee,
+        total: flightCost + lodgingCost + agentFee
+      }
+      return costBreakdown;
+    }
+  return 'Please enter a valid trip ID.';
+  }
 }
 
 export default Traveler;
